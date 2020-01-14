50f4950cc181db20934d92c0a966539a7c697b5c
HomeReinkaos <ds2lvg@gmail.com>
2020-01-14 14:27:22 +0900

    useAsync 커스텀 Hook 만들어서 사용하기 - 처음에 데이터 로딩시

--- src/Users.js
+++ src/Users.js
@@ -1,69 +1,20 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React from 'react';
 import axios from 'axios';
+import useAsync from './useAsync';
 
-const LOADING = 'LOADING';
-const SUCCESS = 'SUCCESS';
-const ERRPR = 'ERRPR';
-
-const initialState = {
-  loading: false,
-  data: null,
-  error: null,
-}
-
-function reducer(state, action) {
-  console.log({...state})
-  switch (action.type) {
-    case LOADING: // 로딩중 켜고 나머지는 상태 그대로
-      return {
-        ...state, // data: null, error: null,
-        loading: true,
-      }
-    case SUCCESS: // 로딩중 끄고, 결과 데이터를 data에 할당.
-      return { 
-        ...state, // error: null,
-        loading: false,
-        data: action.data,
-      };
-    case ERRPR: // 로딩중 끄고, error객체 할당
-      return {
-        ...state, // data: null,
-        loading: false,
-        error: action.error,
-      };
-    default:
-      // return state; 보통 state를 반환하게 하는데, 액션 타입 맞는게 없다는 것이니 Error를 반환하는 것이 로직상 맞다.
-      throw new Error(`Unhandled Action Type: ${action.type}`);
-  }
+async function getUsers() {
+  const url = 'https://jsonplaceholder.typicode.com/users';
+  const response = await axios.get(url);
+  return response.data;
 }
 
 const Users = () => {
-  // 사용할 reducer와 초기상태를 대입해서 관리한다.
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const fetchUsers = async () => {
-    dispatch({type: LOADING});
-    try {
-      const url = 'https://jsonplaceholder.typicode.com/users';
-      const response = await axios.get(url);
-      dispatch({type: SUCCESS, data: response.data}); 
-    } catch (e) {
-      console.dir(e.toJSON());
-      dispatch({type: ERRPR, error: e})
-    }
-  };
-
-  useEffect(() => {
-    // 초기 API 요청
-    fetchUsers();
-  }, []);
-
+  const [state, refetch] = useAsync(getUsers, []); // 빈배열은 처음 실행할때 한번만 호출한다는 것
   const {loading, data: users, error} = state;
   // 로딩중일 때
   if(loading) return <div>로딩중</div>
   // 에러났을 때
   if(error) {
-    // 숫자가 아닌 문자열은 없애버리면 에러코드만 남음
-    // const errCode = error.message.replace(/[^\d]/g, "");
     const errCode = error.response.status
     return (
       <>
@@ -81,7 +32,7 @@ const Users = () => {
     <ul>
       {users.map(user => <li key={user.id}>{user.username} {user.name}</li>)}
     </ul>
-    <button onClick={fetchUsers}>API 재요청</button>
+    <button onClick={refetch}>API 재요청</button>
     </>
   );
 };
---
+++ src/useAsync.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useReducer, useCallback } from 'react';
+
+const initialState = {
+  loading: false,
+  data: null,
+  error: null,
+}
+
+const LOADING = 'LOADING';
+const SUCCESS = 'SUCCESS';
+const ERRPR = 'ERRPR';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case LOADING: // 로딩중 켜고 나머지는 상태 그대로
+      return {
+        ...state, // data: null, error: null,
+        loading: true,
+      }
+    case SUCCESS: // 로딩중 끄고, 결과 데이터를 data에 할당.
+      return { 
+        ...state, // error: null,
+        loading: false,
+        data: action.data,
+      };
+    case ERRPR: // 로딩중 끄고, error객체 할당
+      return {
+        ...state, // data: null,
+        loading: false,
+        error: action.error,
+      };
+    default:
+      // return state; 보통 state를 반환하게 하는데, 액션 타입 맞는게 없다는 것이니 Error를 반환하는 것이 로직상 맞다.
+      throw new Error(`Unhandled Action Type: ${action.type}`);
+  }
+}
+
+// callback: API호출함수, deps: useEffect 두번째 파라미터로 사용하는 []
+function useAsync(callback, deps=[]) {
+  // 사용할 reducer와 초기상태를 대입해서 관리한다.
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // callback 함수가 바뀔 경우만 새로 생성해서 실행
+  const fetchData = useCallback(async () => {
+    dispatch({ type: 'LOADING'});
+    try {
+      const data = await callback();
+      dispatch({ type: SUCCESS, data });
+    } catch (e) {
+      dispatch({ type: ERRPR, error: e });
+    }
+  }, [callback]);
+
+  useEffect(() => {
+    fetchData();
+    // eslint-disable-next-line
+  }, deps);
+
+  return [state, fetchData]; // 상태, API요청함수 반환
+}
+
+export default useAsync;
