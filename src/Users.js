9dac2d7b730394a906fc2c43917371e3d09f5083
HomeReinkaos <ds2lvg@gmail.com>
2020-01-14 13:08:51 +0900

    useReducer로 요청 상태 관리

--- src/Users.js
+++ src/Users.js
@@ -1,26 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import axios from 'axios';
 
+const LOADING = 'LOADING';
+const SUCCESS = 'SUCCESS';
+const ERRPR = 'ERRPR';
+
+const initialState = {
+  loading: false,
+  data: null,
+  error: null,
+}
+
+function reducer(state, action) {
+  console.log({...state})
+  switch (action.type) {
+    case LOADING: // 로딩중 켜고 나머지는 상태 그대로
+      return {
+        ...state, // data: null, error: null,
+        loading: true,
+      }
+    case SUCCESS: // 로딩중 끄고, 결과 데이터를 data에 할당.
+      return { 
+        ...state, // error: null,
+        loading: false,
+        data: action.data,
+      };
+    case ERRPR: // 로딩중 끄고, error객체 할당
+      return {
+        ...state, // data: null,
+        loading: false,
+        error: action.error,
+      };
+    default:
+      // return state; 보통 state를 반환하게 하는데, 액션 타입 맞는게 없다는 것이니 Error를 반환하는 것이 로직상 맞다.
+      throw new Error(`Unhandled Action Type: ${action.type}`);
+  }
+}
+
 const Users = () => {
-  const [users, setUsers] = useState(null);      // 요청의 결과
-  const [loading, setLoading] = useState(false); // 로딩 상태
-  const [error, setError] = useState(null);      // 에러 핸들링
-
+  // 사용할 reducer와 초기상태를 대입해서 관리한다.
+  const [state, dispatch] = useReducer(reducer, initialState);
   const fetchUsers = async () => {
+    dispatch({type: LOADING});
     try {
-      // 초기화
-      setUsers(null);
-      setError(null);
-      setLoading(true);
-
       const url = 'https://jsonplaceholder.typicode.com/users';
       const response = await axios.get(url);
-      setUsers(response.data);
+      dispatch({type: SUCCESS, data: response.data}); 
     } catch (e) {
       console.dir(e.toJSON());
-      setError(e);
+      dispatch({type: ERRPR, error: e})
     }
-    setLoading(false); // 로딩이 끝난 것을 알림
   };
 
   useEffect(() => {
@@ -28,7 +57,10 @@ const Users = () => {
     fetchUsers();
   }, []);
 
+  const {loading, data: users, error} = state;
+  // 로딩중일 때
   if(loading) return <div>로딩중</div>
+  // 에러났을 때
   if(error) {
     // 숫자가 아닌 문자열은 없애버리면 에러코드만 남음
     // const errCode = error.message.replace(/[^\d]/g, "");
@@ -41,8 +73,9 @@ const Users = () => {
       </>
     )
   }
+  // 데이터가 없을때
   if(!users) return null;
-
+  // 성공했을 때
   return (
     <>
     <ul>
